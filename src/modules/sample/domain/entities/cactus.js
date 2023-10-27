22178efe3476cbdf1a51d5fd8959603a2fce1caf
David Escribano Rodríguez <david.escribanor@gmail.com>
2023-10-27 23:54:26 +0200

    domain files

---
+++ src/modules/sample/domain/entities/cactus.js
@@ -0,0 +1,12 @@
+class Cactus {
+  constructor(id, name, species, age, description, image) {
+    this.id = id
+    this.name = name
+    this.species = species
+    this.age = age
+    this.description = description
+    this.image = image
+  }
+}
+
+module.exports = Cactus
---
+++ src/modules/sample/domain/repositories/cactus-repository.js
@@ -0,0 +1,17 @@
+class CactusRepository {
+  create (cactus) {
+    // Implementación para guardar un cactus en la base de datos
+  }
+
+  update (cactus) {
+    // Implementación para actualizar un cactus en la base de datos
+  }
+
+  getById (id) {
+    // Implementación para obtener un cactus por id
+  }
+
+  // Otros metodos del repositorio
+}
+
+module.exports = CactusRepository
