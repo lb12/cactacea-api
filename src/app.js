574518eb965b5f180c4f3285d57900b0654f3362
David Escribano Rodríguez <david.escribanor@gmail.com>
2023-10-28 00:57:15 +0200

    accept json body

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const cactusRouter = require('./modules/sample/interfaces/routes/cactus-routes')
 
 app.get('/', (req, res) => {
@@ -11,7 +14,6 @@ app.get('/', (req, res) => {
 })
 
 app.use(cactusRouter)
-
 
 const PORT = process.env.PORT || 5010
 
